dfe6587c45c65bb3115170f5207537dd9641adb8
Flynn <wphestiraid@gmail.com>
2018-10-20 22:53:21 +0900

    Saving the Notes Offline

--- src/clientState.js
+++ src/clientState.js
@@ -1,15 +1,9 @@
 import { NOTE_FRAGMENT } from './fragments';
 import { GET_NOTES } from './queries';
+import { saveNotes } from './offline';
 
 export const defaults = {
-  notes: [
-    {
-      __typename: 'Note',
-      id: 1,
-      title: 'FIRST',
-      content: 'Content'
-    }
-  ]
+  notes: []
 };
 export const typeDefs = [
   `
@@ -48,6 +42,7 @@ export const resolvers = {
           notes: [newNote, ...notes]
         }
       });
+      saveNotes(cache);
       return newNote;
     },
     editNote: (_, { id, title, content }, { cache }) => {
@@ -66,6 +61,7 @@ export const resolvers = {
         fragment: NOTE_FRAGMENT,
         data: updatedNote
       });
+      saveNotes(cache);
       return updatedNote;
     }
   },
